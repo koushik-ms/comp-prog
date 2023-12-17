2eb380d2eab37d85ba07058c5a284e091a157f99
Meenakshisundaram Koushik <koushik.ms@gmail.com>
2023-12-18 02:04:59 +0530

    optimize solution for day17

--- python/aoc2023/day17/day17.py
+++ python/aoc2023/day17/day17.py
@@ -1,5 +1,5 @@
 from .p01 import al
-from collections import deque
+from heapq import heappush, heappop
 
 DR = [1, 0, -1, 0]
 DC = [0, -1, 0, 1]
@@ -19,8 +19,12 @@ def part1():
 1224686865563
 2546548887735
 4322674655533""".split("\n")
-    print(mp(ts))
-    print(mp(al))
+    pt = mp(ts)
+    print(pt)
+    assert pt == 102
+    pa = mp(al)
+    print(pa)
+    assert pa == 970
 
 def part2():
     print("AoC 2023: 17.2")
@@ -29,7 +33,9 @@ def part2():
 999999999991
 999999999991
 999999999991""".split("\n")
-    print(up(ts))
+    pt = up(ts)
+    print(pt)
+    assert pt == 71
     ts = """2413432311323
 3215453535623
 3255245654254
@@ -43,27 +49,29 @@ def part2():
 1224686865563
 2546548887735
 4322674655533""".split("\n")
-    print(up(ts))
-    print(up(al))
+    pt = up(ts)
+    print(pt)
+    assert pt == 94
+    pa = up(al)
+    print(pa)
+    assert pa == 1149
 
 def up(mat):
     G = [[int(c) for c in r] for r in mat]
     h = len(G)
     w = len(G[0]) if h else 0
-    pp = deque()
-    pp.append((0, 0, 3, 0, 0)) # , (0, 0, 3, 0, 0)
-    pp.append((0, 0, 0, 0, 0)) # , (0, 0, 3, 0, 0)
+    pp = []   # cost, row, col, direction, runlength
+    heappush(pp, (0, 0, 0, 3, 0))
+    heappush(pp, (0, 0, 0, 0, 0))
     seen = set()
-    sl = {}
+    cost_profile = {}
     ans = -1
     while pp:
-        i, j, d, cost, run = pp.popleft()
-        if (i, j , d, run) in seen:
-            pc = sl[(i, j, d, run)]
-            if cost >= pc:
-                continue
+        cost, i, j, d, run = heappop(pp)
+        if (i, j , d, run) in seen and cost >= cost_profile[(i, j, d, run)]:
+            continue
         seen.add((i, j, d, run))
-        sl[(i, j, d, run)] = cost
+        cost_profile[(i, j, d, run)] = cost
         if i == (h-1) and j == (w-1):
             if run > 3 and (ans == -1 or ans > cost):
                 ans = cost
@@ -80,43 +88,43 @@ def up(mat):
             if 0<=ni<h and 0<=nj<w:
                 nc = cost+G[ni][nj]
                 nr = run+1 if d == nd else 1
-                if (ni, nj, nd, nr) not in seen or nc < sl[(ni, nj, nd, nr)]:
-                    pp.append((ni, nj, nd, nc, nr))
+                if (ni, nj, nd, nr) not in seen or nc < cost_profile[(ni, nj, nd, nr)]:
+                    heappush(pp, (nc, ni, nj, nd, nr))
     return ans
 
 def mp(mat):
     G = [[int(c) for c in r] for r in mat]
     h = len(G)
     w = len(G[0]) if h else 0
-    pp = deque()
-    pp.append((0, 0, 3, 0, 0))
+    pp = []   # cost, row, col, direction, runlength
+    heappush(pp, (0, 0, 0, 3, 0))
     seen = set()
-    sl = {}
+    cost_profile = {}
     ans = -1
     while pp:
-        i, j, d, cost, run = pp.popleft()
+        cost, i, j, d, run = heappop(pp)
         if (i, j , d) in seen:
-            (pc, pr) = sl[(i, j, d)]
+            (pc, pr) = cost_profile[(i, j, d)]
             if cost >= pc and run >= pr:
                 continue
         seen.add((i, j, d))
-        sl[(i, j, d)] = (cost, run)
+        cost_profile[(i, j, d)] = (cost, run)
         if i == (h-1) and j == (w-1):
             ans = cost if (ans == -1) else min(ans, cost)
             continue
-        for nd in [d, (d+1)%4, (d+3)%4]:
+        dl = [(d+1)%4, (d+3)%4]
+        if run < 2:
+            dl.append(d)
+        for nd in dl:
             ni = i+DR[nd]
             nj = j+DC[nd]
             if 0<=ni<h and 0<=nj<w:
                 if (ni, nj, nd) in seen:
-                    pc, pr = sl[(ni, nj, nd)]
+                    pc, pr = cost_profile[(ni, nj, nd)]
                     if pc <= cost and pr <= run:
                         continue
-                if d == nd:
-                    if run < 2:
-                        pp.append((ni, nj, nd, cost+G[ni][nj], run+1))
-                else:
-                    pp.append((ni, nj, nd, cost+G[ni][nj], 0))
+                nr = (run+1) if d == nd else 0
+                heappush(pp, (cost+G[ni][nj], ni, nj, nd, nr))
     return ans
 
 if __name__ == "__main__":
