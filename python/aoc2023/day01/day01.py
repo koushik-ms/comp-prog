2a05bb3d5a64c147a179b2647bda6a454a1212d5
Meenakshisundaram Koushik <koushik.ms@gmail.com>
2024-04-13 23:43:14 +0530

    Fix linter warnings in day1 & run_all.sh

--- python/aoc2023/day01/day01.py
+++ python/aoc2023/day01/day01.py
@@ -1,13 +1,15 @@
 from .p01 import al
 
+
 def part1():
     print("AoC 2023: 1.1")
     print(val("pqr3stu8vwx"))
-    assert(val("pqr3stu8vwx") == 38)
-    tl = ["1abc2", "pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"]
+    assert val("pqr3stu8vwx") == 38
+    tl = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
     print(cvs(tl))
-    assert(cvs(tl) == 142)
+    assert cvs(tl) == 142
     print(cvs(al))
+
 
 def part2():
     print("AoC 2023: 1.2")
@@ -19,9 +21,12 @@ abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen""".split("\n")
+7pqrstsixteen""".split(
+        "\n"
+    )
     print(cvs(tl, cal))
     print(cvs(al, cal))
+
 
 v = {
     "one": "1",
@@ -35,12 +40,15 @@ v = {
     "nine": "9",
 }
 tokens = "1 2 3 4 5 6 7 8 9 0 one two three four five six seven eight nine".split()
+
+
 def cal(s):
     def f(c):
         if c in v:
             return v[c]
         else:
             return c
+
     ind = [(s.find(e), e) for e in tokens]
     fin = sorted([e for e in ind if e[0] >= 0])
     ind = [(s.rfind(e), e) for e in tokens]
@@ -48,12 +56,15 @@ def cal(s):
     r = f(fin[0][-1]) + f(rin[-1][-1])
     return int(r)
 
+
 def val(s):
     ns = [c for c in s if c.isnumeric()]
     return int(ns[0] + ns[-1])
 
-def cvs(l, op=val):
-    return sum([op(e) for e in l])
+
+def cvs(items, op=val):
+    return sum([op(e) for e in items])
+
 
 if __name__ == "__main__":
     part1()
--- python/aoc2023/run_all.sh
+++ python/aoc2023/run_all.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 for d in day*; do
-  echo === Running solution for ${d} ===
-  time python -m "${d}.${d}"
-  echo ==================================
+	echo "=== Running solution for ${d} ==="
+	time python3 -m "${d}.${d}"
+	echo "=================================="
 done
-
